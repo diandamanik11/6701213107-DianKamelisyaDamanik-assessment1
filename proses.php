3e6f6d3c8b3ec39b11f25d161d31c294fe9a226c
diandamanik11 <diandamanik18@gmail.com>
2023-03-30 09:58:05 +0700

    v1.3 isi proses

---
+++ proses.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$angka1 = $_POST["angka1"];
+	$angka2 = $_POST["angka2"];
+	$operasi = $_POST["operasi"];
+
+	if ($operasi == "+") {
+		$hasil = $angka1 + $angka2;
+		echo "Hasil penambahan: " . $hasil;
+	} else if ($operasi == "-") {
+		$hasil = $angka1 - $angka2;
+		echo "Hasil pengurangan: " . $hasil;
+	} else if ($operasi == "*") {
+		$hasil = $angka1 * $angka2;
+		echo "Hasil perkalian: " . $hasil;
+	} else {
+		echo "Operasi tidak valid.";
+	}
+}
+
+
+
+?>
